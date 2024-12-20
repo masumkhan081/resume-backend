1fdafd9ce15e0bfc45afd0327884f264faaec952
masumkhan081 <masumkhan081@gmail.com>
2024-12-20 22:27:37 +0600

    js way. memory.attempted to do it using react. after coredevs

--- js-to-ts/src/schemas/user.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const userSchema = z.object({
-  name: z
-    .string()
-    .min(1, { message: "Name is required" })
-    .max(50, { message: "Name cannot exceed 50 characters" }),
-});
